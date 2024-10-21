6c034363c52288adad8c36632e07b9cdde6a6cec
Johann Heuer <127251883+johannfh@users.noreply.github.com>
2024-10-21 09:32:02 +0200

    formatting and remove Dashstring()

--- assert/assert.go
+++ assert/assert.go
@@ -10,8 +10,8 @@ import (
 	"github.com/johannfh/go-utils/helpers"
 )
 
-type AssertData interface{
-    Dump() string
+type AssertData interface {
+	Dump() string
 }
 
 // all of the collected [AssertData]
@@ -19,121 +19,119 @@ var assertData map[string]AssertData = map[string]AssertData{}
 
 // add data which will be logged when an assert fails
 func AddAssertData(key string, value AssertData) {
-    assertData[key] = value
+	assertData[key] = value
 }
 
 // remove data from being logged when an assert fails
 func RemoveAssertData(key string) {
-    delete(assertData, key)
+	delete(assertData, key)
 }
 
 // aggregates all of the log data and prints to [os.Stderr]
 // then ends with a call to [os.Exit], stopping the program
 func runAssert(msg string, args ...any) {
 
-    slogValues := []any{
-        "msg", msg,
-        "area", "Assert",
-    }
-    slogValues = append(slogValues, args...)
-    fmt.Fprintf(os.Stderr, "ARGS: %+s\n", args)
+	slogValues := []any{
+		"msg", msg,
+		"area", "Assert",
+	}
+	slogValues = append(slogValues, args...)
+	fmt.Fprintf(os.Stderr, "ARGS: %+s\n", args)
 
-    for k, v := range assertData {
-        slogValues = append(slogValues, k, v.Dump())
-    }
+	for k, v := range assertData {
+		slogValues = append(slogValues, k, v.Dump())
+	}
 
-    fmt.Fprintf(os.Stderr, "ASSERT\n")
-    for i := 0; i < len(slogValues); i += 2 {
-        fmt.Fprintf(os.Stderr, "\t%s=%v\n", slogValues[i], slogValues[i + 1])
-    }
+	fmt.Fprintf(os.Stderr, "ASSERT\n")
+	for i := 0; i < len(slogValues); i += 2 {
+		fmt.Fprintf(os.Stderr, "\t%s=%v\n", slogValues[i], slogValues[i+1])
+	}
 
-    fmt.Fprintln(os.Stderr, string(debug.Stack()))
-    os.Exit(1)
+	fmt.Fprintln(os.Stderr, string(debug.Stack()))
+	os.Exit(1)
 }
 
 // errors if [truth] is `false`
 func Assert(truth bool, msg string, data ...any) {
-    slog.Info("Assert Check", "truth", truth)
-    if !truth {
-        slog.Error("Assert#false encountered")
-        runAssert(msg, data...)
-    }
+	slog.Info("Assert Check", "truth", truth)
+	if !truth {
+		slog.Error("Assert#false encountered")
+		runAssert(msg, data...)
+	}
 }
 
 // errors if called
 func Never(msg string, data ...any) {
-    slog.Error("Never#never called")
-    runAssert(msg, data...)
+	slog.Error("Never#never called")
+	runAssert(msg, data...)
 }
 
 // errors if an error is encountered
 func NoError(err error, msg string, data ...any) {
-    slog.Info("No Error Check", "err", err)
-    if err != nil {
-        slog.Error("NoError#error encountered")
-        data = append(data, "err", err)
-        runAssert(msg, data...)
-    }
+	slog.Info("No Error Check", "err", err)
+	if err != nil {
+		slog.Error("NoError#error encountered")
+		data = append(data, "err", err)
+		runAssert(msg, data...)
+	}
 }
 
 // errors if [a] is not equal to [b]
 func Equal[T comparable](a, b T, msg string, data ...any) {
-    slog.Info("Equal Check", "a", a, "b", b)
+	slog.Info("Equal Check", "a", a, "b", b)
 
-    if a != b {
-        slog.Error("Equal#not equal encountered")
-        runAssert(msg, data...)
-    }
+	if a != b {
+		slog.Error("Equal#not equal encountered")
+		runAssert(msg, data...)
+	}
 }
 
 // errors if [a] is equal to [b]
 func NotEqual[T comparable](a, b T, msg string, data ...any) {
-    slog.Info("Not Equal Check", "a", a, "b", b)
+	slog.Info("Not Equal Check", "a", a, "b", b)
 
-    if a == b {
-        slog.Error("NotEqual#equal encountered")
-        runAssert(msg, data...)
-    }
+	if a == b {
+		slog.Error("NotEqual#equal encountered")
+		runAssert(msg, data...)
+	}
 }
 
 // errors if [s] is not empty
 func Empty(s string, msg string, data ...any) {
-    slog.Info("Empty Check", "s", s)
+	slog.Info("Empty Check", "s", s)
 
-    if !helpers.IsEmpty(s) {
-        slog.Error("EmptyCheck#not empty encountered")
-        runAssert(msg, data...)
-    }
+	if !helpers.IsEmpty(s) {
+		slog.Error("EmptyCheck#not empty encountered")
+		runAssert(msg, data...)
+	}
 }
 
 // errors if [s] is empty
 func NotEmpty(s string, msg string, data ...any) {
-    slog.Info("Not Empty Check", "s", s)
+	slog.Info("Not Empty Check", "s", s)
 
-    if helpers.IsEmpty(s) {
-        slog.Error("NotEmpty#empty encountered")
-        runAssert(msg, data...)
-    }
+	if helpers.IsEmpty(s) {
+		slog.Error("NotEmpty#empty encountered")
+		runAssert(msg, data...)
+	}
 }
 
 // errors if [item] is not `nil`
 func Nil(item any, msg string, data ...any) {
-    slog.Info("Nil Check", "item", item)
+	slog.Info("Nil Check", "item", item)
 
-    if item != nil {
-        slog.Error("Nil#not nil encountered")
-        runAssert(msg, data...)
-    }
+	if item != nil {
+		slog.Error("Nil#not nil encountered")
+		runAssert(msg, data...)
+	}
 }
-
 
 // errors if [item] is `nil`
 func NotNil(item any, msg string, data ...any) {
-    slog.Info("Not Nil Check", "item", item)
+	slog.Info("Not Nil Check", "item", item)
 
-    if item == nil || reflect.ValueOf(item).Kind() == reflect.Ptr && reflect.ValueOf(item).IsNil() {
-        slog.Error("NotNil#nil encountered")
-        runAssert(msg, data...)
-    }
+	if item == nil || reflect.ValueOf(item).Kind() == reflect.Ptr && reflect.ValueOf(item).IsNil() {
+		slog.Error("NotNil#nil encountered")
+		runAssert(msg, data...)
+	}
 }
-
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -7,35 +7,26 @@ import (
 	"github.com/johannfh/go-utils/internal/utils"
 )
 
-
-// Check wether or not the object is empty
 func Empty(object interface{}) bool {
-    return utils.IsEmpty(object)
+	return utils.IsEmpty(object)
 }
 
 func NotEmpty(object interface{}) bool {
-    return !utils.IsEmpty(object)
-}
-
-// Generate a string consisting of '-'
-// with the provided length
-func Dashstring(length int) string {
-    return strings.Repeat(string('-'), length)
+	return !utils.IsEmpty(object)
 }
 
 // Add a padding string to the left of every
 // non empty or whitespace line of the string
 func PrependString(str string, pre string) string {
-    lines := strings.Split(str, "\n")
+	lines := strings.Split(str, "\n")
 
-    for i := range lines {
-        if len(strings.TrimSpace(lines[i])) == 0 {
-            continue
-        }
+	for i := range lines {
+		if len(strings.TrimSpace(lines[i])) == 0 {
+			continue
+		}
 
-        lines[i] = fmt.Sprintf("%s%s", pre, lines[i])
-    }
+		lines[i] = fmt.Sprintf("%s%s", pre, lines[i])
+	}
 
-    return strings.Join(lines, "\n")
+	return strings.Join(lines, "\n")
 }
-
